2b30822f6f53c509b069d9fc7672ddfd5f650f94
MatFoxDie <MatFoxDie@MATHEUS>
2023-11-26 13:35:35 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Classes/Funcionarios/Funcionario.vb
@@ -0,0 +1,9 @@
+﻿Public Class Funcionario
+    Public Property nome As String
+    Public Property cpf As String
+    Public Property salario As Double
+
+    Public Function ObterBoficacao() As Double
+        Return salario * 0.1
+    End Function
+End Class
---
+++ ByteBank/Forms/Frm_Principal.vb
@@ -0,0 +1,25 @@
+﻿Public Class Frm_Principal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        Me.Text = "Principal"
+        Lbl_Principal.Text = "Principal"
+
+    End Sub
+
+    Private Sub Frm_Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn_click_Click(sender As Object, e As EventArgs) Handles btn_click.Click
+        Dim funcionario As New Funcionario
+        funcionario.nome = "Matheus"
+        funcionario.cpf = "111.222.333-50"
+        funcionario.salario = 10000
+
+        MsgBox("O salario do " + funcionario.nome + " é " + funcionario.salario.ToString)
+        MsgBox("A bonificação dele será de " + funcionario.ObterBoficacao.ToString)
+    End Sub
+End Class
